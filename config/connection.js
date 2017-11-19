f694e6585e24a42a2e12ebe56b69714a0120042e
Michelle Hamdani <michellehamdani@gmail.com>
2017-11-18 16:52:06 -0800

    bug fix

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,29 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
-
-var connection;
+const mysql = require('mysql');
+let connection = '';
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: " ",
-  database: "tamagotchi_db"
- });
-};
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tamagotchi_db'
+  });
+
+}
+
 
 // Make connection.
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
