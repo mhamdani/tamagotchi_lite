66ff4e6f8506ef8d68475902743a4eb1448c09cc
Michelle Hamdani <michellehamdani@gmail.com>
2017-11-18 17:11:06 -0800

    port update 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-var port = 3306;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
